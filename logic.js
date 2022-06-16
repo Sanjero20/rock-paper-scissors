31c37e726efb2f52480cadf34b0d1cf5b146e6ea
Sanjero20 <gelosantos72@gmail.com>
2022-06-16 11:50:03 +0800

    Make the game run for 5 times

--- logic.js
+++ logic.js
@@ -1,3 +1,4 @@
+// A Game or Rock, Paper, and Scissors
 
 function playRound(playerSelection, computerSelection) {
   let winner;
@@ -37,28 +38,45 @@ function playRound(playerSelection, computerSelection) {
     }
   }
 
+  if (winner == "Player") {
+    userScore += 1;
+  }
+  else if (winner == "Computer") {
+    compScore += 1;
+  }
+
   return winner;
 }
 
 function computerPlay() {
-  let choices = ["rock", "paper", "scissors"];
   let length = choices.length;
 
   let pick = Math.floor(Math.random() * length)
   return choices[pick]
 }
 
+function playerTurn() {
+  let user = prompt("Rock, Paper, Scissors: ");
+  if (choices.includes(user)) {
+    user = user.toLowerCase();
+  }
+  return user;
+}
+
+function game() {
+  let user, comp, winner;
+
+  for (let i=1; i <= 5; i++) {
+    user = playerTurn();
+    comp = computerPlay();
+    winner = playRound(user, comp);
+    console.log(winner);
+  }
+}
 
 // Driver Code
-let bot;
-let x;
+let choices = ["rock", "paper", "scissors"];
+let userScore = 0;
+let compScore = 0;
 
-let user = prompt("Rock, Paper, Scissors: ");
-if (user) {
-  user = user.toLowerCase();
-  bot = computerPlay();
-  x = playRound(user, bot);
-
-  console.log(`Player plays "${user.toUpperCase()}" and Computer plays "${bot.toUpperCase()}"`);
-  console.log(`${x} wins`);
-}
+game();
